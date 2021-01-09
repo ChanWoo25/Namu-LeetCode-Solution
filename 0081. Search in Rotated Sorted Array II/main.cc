0fd5eb69fd18bf705d1f333e8df50eee31467556
Chanwoo Lee <leechanwoo25@gmail.com>
2021-01-10 01:06:20 +0900

    Solve 81

--- 0081. Search in Rotated Sorted Array II/main.cc
+++ 0081. Search in Rotated Sorted Array II/main.cc
@@ -6,34 +6,85 @@ public:
     bool search(vector<int>& nums, int target) {
         int n = nums.size();
 
-        int lo, hi, m, pivot;
-        if(nums[0] >= nums[n-1]) 
-        {   // pivot exists. Find It!
-            lo = 0, hi = n-1;
-            while(lo < hi)
-            {
-                m = (lo+hi)/2;
-                if(nums[m] <= nums[hi])
-                    hi = m;
-                else if(nums[lo] <= nums[m])
-                    lo = m + 1;
-            }
-            pivot = lo;
-        }
-        // cout << pivot << endl;
-
-        lo = 0, hi = n - 1;
+        int lo(0), hi(n-1), m;
+        /**When the array was rotated, 
+         * Let's refer to the first sorted array as 'F' and second as 'S'
+         * If nums[m] and target belong to different array, m can be easily updated.
+         * Otherwise, nums[m] and target belongs to the same array.
+         * In that case, we should compare two elements again.
+         */
         while(lo <= hi)
         {
+            while(lo < hi && nums[lo] == nums[lo+1])
+                lo++;
+            while(lo < hi && nums[hi] == nums[hi-1])
+                hi--;
+            
             m = (lo+hi)/2;
-            int rot_m = (m + pivot) % n;
-
-            if(nums[rot_m] < target)
-                lo = m + 1;
-            else if(target < nums[rot_m])
-                hi = m - 1;
-            else
+            if(nums[m] == target) 
                 return true;
+
+            if(nums[lo] <= target)
+            {
+                if(nums[lo] <= nums[m]){
+                    if(nums[m] < target)
+                        lo = m + 1;
+                    else if(target < nums[m])
+                        hi = m - 1;
+                }
+                else
+                    hi = m - 1;
+            }
+            else
+            {
+                if(nums[lo] <= nums[m])
+                    lo = m + 1;
+                else{
+                    if(nums[m] < target)
+                        lo = m + 1;
+                    else if(target < nums[m])
+                        hi = m - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+class Solution1 {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int lo(0), hi(n-1), m;
+        while(lo <= hi)
+        {
+            while(lo < hi && nums[lo] == nums[lo+1])
+                lo++;
+            while(lo < hi && nums[hi] == nums[hi-1])
+                hi--;
+            
+            m = (lo+hi)/2;
+            if(nums[m] == target) return true;
+            
+            bool midInFirst = (nums[lo] <= nums[m]);
+            bool tgtInFirst = (nums[lo] <= target);
+            
+            if(midInFirst ^ tgtInFirst)
+            {
+                if(midInFirst)
+                    lo = m + 1;
+                else
+                    hi = m - 1;
+            }
+            else
+            {
+                if(nums[m] < target)
+                    lo = m + 1;
+                else
+                    hi = m - 1;
+            }
         }
         return false;
     }
