15ae6f939cb1d03b4f08c905a72947e08573b5f8
Chanwoo Lee <leechanwoo25@gmail.com>
2021-01-22 17:08:08 +0900

    Update 230

--- 0230. Kth Smallest Element in a BST/README.md
+++ 0230. Kth Smallest Element in a BST/README.md
@@ -62,3 +62,92 @@ public:
     }
 };
 ```
+
+#### Follow-up
+- 만약 Tree에 지속적으로 원소가 추가될 수 있다면,
+- 어떻게 더 효율적으로 관리할 수 있을까?
+
+```cpp
+class Solution {
+private:    
+    typedef struct TreeWithNodeCount{
+        int val, count;
+        TreeWithNodeCount *left, *right;
+        TreeWithNodeCount() : val(0), count(1), left(nullptr), right(nullptr) {}
+        TreeWithNodeCount(int x) : val(x), count(1), left(nullptr), right(nullptr) {}
+        TreeWithNodeCount(int x, TreeWithNodeCount* l, TreeWithNodeCount* r) 
+            : val(x), count(1), left(l), right(r) {}
+    } nTree;
+
+    nTree* buildnTree(TreeNode* root)
+    {
+        if(!root)
+            return nullptr;
+        
+        nTree* curr = new nTree(root->val, 
+                                buildnTree(root->left), 
+                                buildnTree(root->right));
+        
+        curr->count += (curr->left)?(curr->left->count):0;
+        curr->count += (curr->right)?(curr->right->count):0;
+        return curr;
+    }
+
+    // Class's root nTree
+    nTree* nRoot;
+
+public:
+    int findKthSmallest(nTree* root, int k)
+    {
+        // Size Exception
+        if(k <= 0 || root->count < k) return -1;
+
+        if(root->left)
+        {
+            int leftNodes = root->left->count;
+            if(k <= leftNodes)
+                return findKthSmallest(root->left, k);
+            else if(k-1 == leftNodes)
+                return root->val;
+            else
+                return findKthSmallest(root->right, k-1-leftNodes);
+        }
+        else
+        {
+            if(k == 1)
+                return root->val;
+            else
+                return findKthSmallest(root->right, k-1);
+        }
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        nRoot = buildnTree(root);
+        return findKthSmallest(nRoot, k);
+    }
+
+    // We don't consider 
+    // the case that redundunt e aleady exists in nRoot.
+    void addNewNode(int e)
+    {
+        if(!nRoot){
+            cout << "Make nRoot First!" << endl;
+            return;
+        }
+
+        nTree* p = nRoot;
+        while(p)
+        {
+            if(p->val < e){
+                p->count++;
+                p = p->right;
+            }
+            else{
+                p->count++;
+                p = p->left;
+            }
+        }
+        p = new nTree(e);
+    }
+};
+```
--- 0230. Kth Smallest Element in a BST/main.cc
+++ 0230. Kth Smallest Element in a BST/main.cc
@@ -55,66 +55,124 @@ public:
     }
 };
 
-typedef struct NumTreeNode{
-    int val;
-    NumTreeNode *left, *right;
-    NumTreeNode() : val(0), left(nullptr), right(nullptr) {}
-    NumTreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-} nTree;
+// public class Solution {
+//     public int kthSmallest(TreeNode root, int k) {
+//         TreeNodeWithCount rootWithCount = buildTreeWithCount(root);
+//         return kthSmallest(rootWithCount, k);
+//     }
+    
+//     private TreeNodeWithCount buildTreeWithCount(TreeNode root) {
+//         if (root == null) return null;
+//         TreeNodeWithCount rootWithCount = new TreeNodeWithCount(root.val);
+//         rootWithCount.left = buildTreeWithCount(root.left);
+//         rootWithCount.right = buildTreeWithCount(root.right);
+//         if (rootWithCount.left != null) rootWithCount.count += rootWithCount.left.count;
+//         if (rootWithCount.right != null) rootWithCount.count += rootWithCount.right.count;
+//         return rootWithCount;
+//     }
+    
+//     private int kthSmallest(TreeNodeWithCount rootWithCount, int k) {
+//         if (k <= 0 || k > rootWithCount.count) return -1;
+//         if (rootWithCount.left != null) {
+//             if (rootWithCount.left.count >= k) return kthSmallest(rootWithCount.left, k);
+//             if (rootWithCount.left.count == k-1) return rootWithCount.val;
+//             return kthSmallest(rootWithCount.right, k-1-rootWithCount.left.count);
+//         } else {
+//             if (k == 1) return rootWithCount.val;
+//             return kthSmallest(rootWithCount.right, k-1);
+//         }
+//     }
+    
+//     class TreeNodeWithCount {
+//         int val;
+//         int count;
+//         TreeNodeWithCount left;
+//         TreeNodeWithCount right;
+//         TreeNodeWithCount(int x) {val = x; count = 1;};
+//     }
+// }
 
-class FailSolution {
+
+class Solution {
 private:    
-    int numOfNodes(TreeNode* root, nTree* nroot)
+    typedef struct TreeWithNodeCount{
+        int val, count;
+        TreeWithNodeCount *left, *right;
+        TreeWithNodeCount() : val(0), count(1), left(nullptr), right(nullptr) {}
+        TreeWithNodeCount(int x) : val(x), count(1), left(nullptr), right(nullptr) {}
+        TreeWithNodeCount(int x, TreeWithNodeCount* l, TreeWithNodeCount* r) 
+            : val(x), count(1), left(l), right(r) {}
+    } nTree;
+
+    nTree* buildnTree(TreeNode* root)
     {
         if(!root)
-            return 0;
-        else
-            nroot = new nTree();
+            return nullptr;
         
-        return nroot->val \
-            = numOfNodes(root->left, nroot->left) \
-            + numOfNodes(root->right, nroot->right) \
-            + 1;
+        nTree* curr = new nTree(root->val, 
+                                buildnTree(root->left), 
+                                buildnTree(root->right));
+        
+        curr->count += (curr->left)?(curr->left->count):0;
+        curr->count += (curr->right)?(curr->right->count):0;
+        return curr;
     }
-public:
-    int kthSmallest(TreeNode* root, int k) {
-        nTree *nroot = new nTree();
-        numOfNodes(root, nroot);
 
-        while(1)
+    // Class's root nTree
+    nTree* nRoot;
+
+public:
+    int findKthSmallest(nTree* root, int k)
+    {
+        // Size Exception
+        if(k <= 0 || root->count < k) return -1;
+
+        if(root->left)
         {
-            if(k==1)
-            {
-                while(root->left)
-                    root = root->left;
+            int leftNodes = root->left->count;
+            if(k <= leftNodes)
+                return findKthSmallest(root->left, k);
+            else if(k-1 == leftNodes)
                 return root->val;
-            }
             else
-            {
-                if(nroot->left)
-                {
-                    if(k <= nroot->left->val){
-                        root = root->left;
-                        nroot = nroot->left;
-                    }
-                    else if(k == (nroot->left->val + 1))
-                        return root->val;
-                    else{
-                        k -= (nroot->left->val + 1);
-                        root = root->right;
-                        nroot = nroot->right;
-                    }
-                }
-                else 
-                {
-                    k -= 1;
-                    root = root->right;
-                    nroot = nroot->right;
-                }
-            }
+                return findKthSmallest(root->right, k-1-leftNodes);
+        }
+        else
+        {
+            if(k == 1)
+                return root->val;
+            else
+                return findKthSmallest(root->right, k-1);
+        }
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        nRoot = buildnTree(root);
+        return findKthSmallest(nRoot, k);
+    }
+
+    // We don't consider 
+    // the case that redundunt e aleady exists in nRoot.
+    void addNewNode(int e)
+    {
+        if(!nRoot){
+            cout << "Make nRoot First!" << endl;
+            return;
         }
 
-        return -1;
+        nTree* p = nRoot;
+        while(p)
+        {
+            if(p->val < e){
+                p->count++;
+                p = p->right;
+            }
+            else{
+                p->count++;
+                p = p->left;
+            }
+        }
+        p = new nTree(e);
     }
 };
 
